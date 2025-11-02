eeacafde321feda3a4155822f9e26c51585eaaf7
Domekologe <github@domekologe.eu>
2025-11-02 13:26:01 +0100

    Update __init__.py

--- WoWTools/__init__.py
+++ WoWTools/__init__.py
@@ -8,5 +8,4 @@ __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 async def setup(bot):
     await bot.add_cog(WoWTools(bot))
     await bot.add_cog(SetupNewMember(bot))
-    await bot.add_cog(SetupNewMember(bot))
     await bot.add_cog(MemberCharSetup(bot))
